a8d3f84e89b026fecc9332212ba06cddc7f5c197
Aryan Sinha <z5589770@ad.unsw.edu.au>
2026-06-30 17:12:23 +1000

    Update vite config to build all HTML files

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,24 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import fs from 'fs';
+
+const htmlFiles = fs.readdirSync('./parklea-site')
+  .filter(file => file.endsWith('.html') && file !== 'template.html' && file !== 'Parkly SFC.html');
+
+const input = {};
+htmlFiles.forEach(file => {
+  const name = file.replace('.html', '');
+  input[name] = resolve(__dirname, 'parklea-site', file);
+});
+
 export default defineConfig({
   root: './parklea-site',
   server: { port: 3000, open: true },
-  build: { outDir: '../dist' },
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input
+    }
+  },
 });
